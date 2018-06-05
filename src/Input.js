74e3eba73d0e87926da5757ecf7285f5626c92f6
shayde <shayde@rnd-hub.com>
2018-06-05 15:43:00 +0300

    add border to input

--- src/Input.js
+++ src/Input.js
@@ -8,7 +8,7 @@ height: 40px;
 margin: 20px;
 padding: 10px;
 font-size: 30px;
-border-width: 0px;
+border: 1px solid black;
 background-color: white;
 border-radius: 5px;
 text-align: center;
@@ -19,57 +19,3 @@ text-align: center;
 `;
 
 export default StyledInput;
-
-
-// import React from 'react';
-// import TextField from '@material-ui/core/TextField';
-// import { withStyles } from '@material-ui/core/styles';
-
-// const styles = theme => ({
-//     bootstrapRoot: {
-//       padding: 0
-//     },
-//     bootstrapInput: {
-//       textAlign: 'center',
-//       borderRadius: 4,
-//       backgroundColor: theme.palette.common.white,
-//       border: '1px solid #ced4da',
-//       fontSize: 20,
-//       padding: '10px 12px',
-//       width: '100%',
-//       transition: theme.transitions.create(['border-color', 'box-shadow']),
-//       fontFamily: [
-//         '-apple-system',
-//         'BlinkMacSystemFont',
-//         '"Segoe UI"',
-//         'Roboto',
-//         '"Helvetica Neue"',
-//         'Arial',
-//         'sans-serif',
-//         '"Apple Color Emoji"',
-//         '"Segoe UI Emoji"',
-//         '"Segoe UI Symbol"',
-//       ].join(','),
-//       '&:focus': {
-//         borderColor: '#80bdff',
-//         boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-//       },
-//     }})
-
-// const Input = ({classes}) => (
-//     <TextField
-//         inputProps={{
-//             fontWidth: 'bold',
-//             fontSize: '10px',
-//         }}
-//         InputProps={{
-//           disableUnderline: true,
-//           classes: {
-//             root: classes.bootstrapRoot,
-//             input: classes.bootstrapInput,
-//           }
-//         }}
-//       />
-// );
-
-// export default withStyles(styles)(Input);
